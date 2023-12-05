510df23bf667eebafd66b40245d2404f5bbe8de7
Timur Sorokin <timursorokin94@gmail.com>
2023-12-05 12:43:20 +0100

    Operaciones claras

--- install.bash
+++ install.bash
@@ -1,7 +1,11 @@
 #!/bin/bash
+echo "-> Instalando entorno virtual"
 python3 -m venv .venv
+echo "->Activando entorno virtual"
 source .venv/bin/activate
-pip install -r requirements.txt
+echo "-> Instalando dependencias"
+pip3 install -r requirements.txt
+echo "-> Deactivando entorno virtual"
 deactivate
 echo "Proceso terminado"
 
